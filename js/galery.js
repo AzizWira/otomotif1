85c7ce9948531d31e38526429759c3b6757f6b4b
rizalkalam <rizalkalam78@gmail.com>
2023-11-11 23:57:08 +0700

    [RK]change assets image from svg to jpg

--- js/galery.js
+++ js/galery.js
@@ -1,35 +1,51 @@
 const data_image = [
     {
-        image: '../assets/galeri/mobil-toyota-alphard.svg',
+        image: '../assets/galeri/galery-alphard.jpg',
         alt: 'Toyota Alphard'
     },
     {
-        image: '../assets/galeri/galery-fortuner.svg',
+        image: '../assets/galeri/galery-fortuner.jpg',
         alt: 'Toyota Fortuner'
     },
     {
-        image: '../assets/galeri/galery-yaris-cross.svg',
+        image: '../assets/galeri/galery-yaris-cross.jpg',
         alt: 'Toyota Yaris Cross'
     },
     {
-        image: '../assets/galeri/galery-calya.svg',
+        image: '../assets/galeri/galery-calya.jpg',
         alt: 'Toyota Calya'
     },
     {
-        image: '../assets/galeri/galery-agya.svg',
+        image: '../assets/galeri/galery-agya.jpg',
         alt: 'Toyota Agya'
     },
     {
-        image: '../assets/galeri/galery-yaris.svg',
+        image: '../assets/galeri/galery-yaris.jpg',
         alt: 'Toyota Yaris'
     },
     {
-        image: '../assets/galeri/galery-innova-zenix.svg',
+        image: '../assets/galeri/galery-avanza.jpg',
+        alt: 'Toyota Avanza'
+    },
+    {
+        image: '../assets/galeri/galery-veloz.jpg',
+        alt: 'Toyota Veloz'
+    },
+    {
+        image: '../assets/galeri/galery-rush.jpg',
+        alt: 'Toyota Rush '
+    },
+    {
+        image: '../assets/galeri/galery-innova-zenix.jpg',
         alt: 'Toyota Innova Zenix'
     },
     {
-        image: '../assets/galeri/galery-innova.svg',
-        alt: 'Toyota Innova '
+        image: '../assets/galeri/galery-innova.jpg',
+        alt: 'Toyota Innova'
+    },
+    {
+        image: '../assets/galeri/galery-raize.jpg',
+        alt: 'Toyota Raize'
     }
 ];
 
